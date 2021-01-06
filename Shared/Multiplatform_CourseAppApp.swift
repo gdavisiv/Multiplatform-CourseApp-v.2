1bd61bb10a8645eb1ad9d5d6648d690cb4e3503b
George Davis IV <gdavisiv@gmail.com>
2021-01-06 10:18:08 -0500

    Initial Commit

---
+++ Shared/Multiplatform_CourseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multiplatform_CourseAppApp.swift
+//  Shared
+//
+//  Created by George Davis IV on 1/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Multiplatform_CourseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
