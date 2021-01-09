72f7df6e133f87be663404b23fb8608000486f11
George Davis IV <gdavisiv@gmail.com>
2021-01-09 18:55:55 -0500

    UIUpdate: Creating New UICard

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -13,10 +13,14 @@ struct ContentView: View {
         VStack(alignment: .leading, spacing: 4.0){
             Image("Illustration 1")
                 .resizable()
-                .aspectRatio(contentMode: .fit)
+                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
             Text("SwiftUI for iOS 14").fontWeight(.bold)
             Text("20 Sections").font(.footnote)
         }
+        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+        .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
     }
 }
 
