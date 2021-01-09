a02da3fb456fb3914595b764841b185f6b66f5ba
George Davis IV <gdavisiv@gmail.com>
2021-01-08 23:48:37 -0500

    UIUpdate: Creating Landing Image/Page

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack(alignment: .leading, spacing: 4.0){
+            Image("Illustration 1")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Text("SwiftUI for iOS 14").fontWeight(.bold)
+            Text("20 Sections").font(.footnote)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView()
+            .preferredColorScheme(.dark)
     }
 }
