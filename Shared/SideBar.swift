81bea31c952d732212d4e652057d845e41f19c09
George Davis IV <gdavisiv@gmail.com>
2021-01-10 21:20:11 -0500

    UIUpdate: Creating New SideBar Menu

---
+++ Shared/SideBar.swift
@@ -0,0 +1,20 @@
+//
+//  SideBar.swift
+//  Multiplatform-CourseApp
+//
+//  Created by George Davis IV on 1/10/21.
+//
+
+import SwiftUI
+
+struct SideBar: View {
+    var body: some View {
+        Label("Courses", systemImage: "book.closed")
+    }
+}
+
+struct SideBar_Previews: PreviewProvider {
+    static var previews: some View {
+        SideBar()
+    }
+}
