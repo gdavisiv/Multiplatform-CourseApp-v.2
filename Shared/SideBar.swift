068ecd7eb0b1da3c3a59a1819de7414df83d57b2
George Davis IV <gdavisiv@gmail.com>
2021-01-10 21:37:40 -0500

    UIUpdate: Creating SideBar Menu with various Functionality

--- Shared/SideBar.swift
+++ Shared/SideBar.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct SideBar: View {
     var body: some View {
-        Label("Courses", systemImage: "book.closed")
+        //This created a Navigation View
+        NavigationView {
+            List {
+                //Provides a Navigation Link from Courses to another View
+                NavigationLink(destination: CoursesView()) {
+                    Label("Courses", systemImage: "book.closed")
+                }
+                Label("Tutorials", systemImage: "list.bullet.rectangle")
+                Label("Livestreams", systemImage: "tv")
+                Label("Certificates", systemImage: "mail.stack")
+                Label("Search", systemImage: "magnifyingglass")
+            }
+            //This sets the list to a SidebarListStyle
+            .listStyle(SidebarListStyle())
+            .navigationTitle("Learn")
+            
+            CoursesView()
+        }
     }
 }
 
