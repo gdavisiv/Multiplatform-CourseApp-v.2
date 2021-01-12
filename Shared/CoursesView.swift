670d8026b7d7335cd9860d27f8cf842fa858dea1
George Davis IV <gdavisiv@gmail.com>
2021-01-11 19:58:06 -0500

    UIUpdate: Updated CoursesView with functionality for iOS/BigSur

--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -8,13 +8,25 @@
 import SwiftUI
 
 struct CoursesView: View {
+    @ViewBuilder
     var body: some View {
+        //Makes code work on iOS Desktop Devices
+        #if os(iOS)
+        content
+            .listStyle(InsetGroupedListStyle())
+        //Code to run on iOS Desktop Devices
+        #else
+        content
+            .frame(minWidth: 800, minHeight: 600)
+        #endif
+    }
+    //Create a mini component
+    var content: some View {
         //Creating a Simple List that iterates 20 times.
         //Calling CourseRow()
         List(0 ..< 20) { item in
             CourseRow()
         }
-        .listStyle(InsetGroupedListStyle())
         //Gives content with a title Above it
         .navigationTitle("Courses")
     }
