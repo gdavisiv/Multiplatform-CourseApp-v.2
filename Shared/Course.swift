c6d816070eb7c534c4878a860ad1aefded94190f
George Davis IV <gdavisiv@gmail.com>
2021-01-15 21:25:01 -0500

    Database: Creating Data Struct

---
+++ Shared/Course.swift
@@ -0,0 +1,16 @@
+//
+//  Course.swift
+//  Multiplatform-CourseApp
+//
+//  Created by George Davis IV on 1/15/21.
+//
+
+import SwiftUI
+
+struct Course: Identifiable {
+    var id = UUID()
+    var title: String
+    var subtitle: String
+    var image: String
+    car color: Color
+}
