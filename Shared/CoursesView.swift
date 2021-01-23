3f359db0d26c2914c9d7cea4cfa6a61967839fbc
George Davis IV <gdavisiv@gmail.com>
2021-01-22 20:42:39 -0500

    UIUpdate: Updated CoursesView with new code implementation and Platform Conditions

--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -18,73 +18,101 @@ struct CoursesView: View {
     
     var body: some View {
         ZStack {
-            ScrollView {
-                //Lazy Grid Allows you to implement columns that will adapt to different screen sizes
-                //It will try to fit as many columns as possible with a minimum width of 100
-                //Depending on the size of the screen
-                LazyVGrid(
-                    columns:
-                        //Array(repeating: .init(.flexible(), spacing: 16), count: 2),
-                        [
-                        GridItem(.adaptive(minimum: 160), spacing: 16)
-                        //GridItem(.adaptive(minimum: 160), spacing: 16)
-                        ],
-                        spacing: 16
-                ) {
-                    ForEach(courses) { item in
-                        VStack {
-                            CourseItem(course: item)
-                                //Allows to animate shared elements between two views
-                                //!show is false because we have to make it true
-                                .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
-                                .frame(height: 200)
-                                .onTapGesture {
-                                    withAnimation(.spring(response: 0.3, dampingFraction: 0.5, blendDuration: 0)) {
-                                        //everytime you tap on the card it will switch between true/false
-                                        show.toggle()
-                                        selectedItem = item
-                                        isDisabled = true
-                                    }
-                                }
-                                //Once you tap a card, you can't tap on others
-                                .disabled(isDisabled)
-                        }
-                        //Creating a container with its own ID
-                        //"container\(item.id)" allows to combine a string & variable
-                        .matchedGeometryEffect(id: "container\(item.id)", in: namespace, isSource: !show)
-                    }
-                    
-                }
-                .padding(16)
-                .frame(maxWidth: .infinity)
-            }
-            //Ensures that the course info does not animated away behind the rest of the cards
-            .zIndex(1)
+            //Set Platform Condition
+            #if os(iOS)
+            content
+                .navigationBarHidden(true)
+            fullContent
+                //Implmented the background blur to background items
+                //Set a custom Blur style
+                .background(VisualEffectBlur(blurStyle: .systemMaterial).edgesIgnoringSafeArea(.all))
             
+            #else
+            content
+            fullContent
+                //Implmented the background blur to background items
+                .background(VisualEffectBlur().edgesIgnoringSafeArea(.all))
             
-            //If selectedItem is not nil
-            //Creates Full Screen Mode
-            if selectedItem != nil {
-                ZStack(alignment: .topTrailing) {
-                    //Passed in the coursem and the namespace
-                    CourseDetail(course: selectedItem!, namespace: namespace)
-                    
-                    CloseButton()
-                        .padding(.trailing, 16)
-                        .onTapGesture {
-                            withAnimation(.spring()) {
-                                //everytime you tap on the card it will switch between true/false
-                                show.toggle()
-                                selectedItem = nil
-                                //DispatchQueue allows a delay in after the click of the card
-                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                                    isDisabled = false
+            #endif
+        }
+        .navigationTitle("Courses")
+    }
+    
+    var content: some View {
+        ScrollView {
+            //Lazy Grid Allows you to implement columns that will adapt to different screen sizes
+            //It will try to fit as many columns as possible with a minimum width of 100
+            //Depending on the size of the screen
+            LazyVGrid(
+                columns:
+                    //Array(repeating: .init(.flexible(), spacing: 16), count: 2),
+                    [
+                    GridItem(.adaptive(minimum: 160), spacing: 16)
+                    //GridItem(.adaptive(minimum: 160), spacing: 16)
+                    ],
+                    spacing: 16
+            ) {
+                ForEach(courses) { item in
+                    VStack {
+                        CourseItem(course: item)
+                            //Allows to animate shared elements between two views
+                            //!show is false because we have to make it true
+                            .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
+                            .frame(height: 200)
+                            .onTapGesture {
+                                withAnimation(.spring(response: 0.3, dampingFraction: 0.5, blendDuration: 0)) {
+                                    //everytime you tap on the card it will switch between true/false
+                                    show.toggle()
+                                    selectedItem = item
+                                    isDisabled = true
                                 }
                             }
-                        }
+                            //Once you tap a card, you can't tap on others
+                            .disabled(isDisabled)
+                    }
+                    //Creating a container with its own ID
+                    //"container\(item.id)" allows to combine a string & variable
+                    .matchedGeometryEffect(id: "container\(item.id)", in: namespace, isSource: !show)
                 }
-                .zIndex(2)
+                
             }
+            .padding(16)
+            .frame(maxWidth: .infinity)
+        }
+        //Ensures that the course info does not animated away behind the rest of the cards
+        .zIndex(1)
+    }
+    
+    
+    @ViewBuilder
+    var fullContent: some View{
+        //If selectedItem is not nil
+        //Creates Full Screen Mode
+        if selectedItem != nil {
+            ZStack(alignment: .topTrailing) {
+                //Passed in the coursem and the namespace
+                CourseDetail(course: selectedItem!, namespace: namespace)
+                
+                CloseButton()
+                    .padding(.trailing, 16)
+                    .onTapGesture {
+                        withAnimation(.spring()) {
+                            //everytime you tap on the card it will switch between true/false
+                            show.toggle()
+                            selectedItem = nil
+                            //DispatchQueue allows a delay in after the click of the card
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                isDisabled = false
+                            }
+                        }
+                    }
+            }
+            .zIndex(2)
+            .frame(maxWidth: 712)
+            //Created a new frame that is set to infinish
+            .frame(maxWidth: .infinity)
+            //Implmented the background blur to background items
+            .background(VisualEffectBlur().edgesIgnoringSafeArea(.all))
         }
     }
 }
